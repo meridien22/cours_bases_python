792c283150ad76cbc8f83c43658dbedd84e452df
meridien22 <meridien.sig@gmail.com>
2025-09-04 08:40:51 +0100

    test

---
+++ calculator/operations.py
@@ -0,0 +1,11 @@
+def sum(left_param, right_param):
+    return left_param + right_param
+
+def sub(left_param, right_param):
+    return left_param - right_param
+
+def mul(left_param, right_param):
+    return left_param * right_param
+
+def div(left_param, right_param):
+    return left_param / right_param
---
+++ main.py
@@ -0,0 +1,5 @@
+import calculator.operations
+
+if __name__ == '__main__':
+    resultat = calculator.operations.mul(10, 10)
+    print(resultat)
