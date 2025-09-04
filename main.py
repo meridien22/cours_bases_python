11bdf3963186ac7c4dd43f240df683b0fc4b8ee5
meridien22 <meridien.sig@gmail.com>
2025-09-04 11:22:41 +0100

    ajustement

--- main.py
+++ main.py
@@ -1,5 +1,25 @@
-from calculator.operations import mul
+import requests
+from bs4 import BeautifulSoup
+import csv
 
-if __name__ == '__main__':
-    resultat = mul(10, 10)
-    print(resultat)
+url = "https://www.gov.uk/search/news-and-communications"
+reponse = requests.get(url)
+page = reponse.content
+soup = BeautifulSoup(page, 'html.parser')
+
+titres = soup.find_all("a", class_="gem-c-subscription-links__item")
+titre_textes = []
+for titre in titres:
+    titre_textes.append(titre.text)
+
+descriptions = soup.find_all("p", class_="gem-c-document-list__item-description")
+description_textes = []
+for description in descriptions:
+    description_textes.append(description.text)
+
+en_tete = ['titre', 'description']
+with open('data.csv','w') as csvfile:
+    writer = csv.writer(csvfile, delimiter=',')
+    writer.writerow(en_tete)
+    for titre, description in zip(titre_textes, description_textes):
+        writer.writerow([titre, description])
