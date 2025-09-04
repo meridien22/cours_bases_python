1c838e456c37a072bb39238d2266f0d3daf10fd0
meridien22 <meridien.sig@gmail.com>
2025-09-04 13:55:42 +0100

    ajustement

--- helloworld.py
+++ helloworld.py
@@ -1,10 +1,37 @@
-print("Hello world")
+import requests
+from bs4 import BeautifulSoup
+import csv
 
-nom = "Dupont"
-prenom = "Jean"
-age = 90
 
-print(f"Bonjour, je m'appelle {prenom} {nom} et j'ai {age} ans.")
+def extraire(url):
+    reponse = requests.get(url)
+    page = reponse.content
 
-print(type(nom))
-print(type(age))
+    # transforme (parse) le HTML en objet BeautifulSoup
+    soup = BeautifulSoup(page, "html.parser")
+    elements = soup.find_all("li", class_="gem-c-document-list__item")
+    return elements
+
+def transformer(element):
+    titre = element.find("a", class_="govuk-link")
+    description = element.find("p", class_="gem-c-document-list__item-description")
+    return (titre.string, description.string)
+
+def charger(donnees):
+    en_tete = ["titre", "description"]
+    # création du fichier data.csv
+    with open("data.csv", "w", newline="") as fichier_csv:
+        writer = csv.writer(fichier_csv, delimiter=",")
+        writer.writerow(en_tete)
+
+        for donnee in donnees:
+            writer.writerow(donnee)
+
+def etl(url):
+    elements = extraire(url)
+    resultats = [transformer(element) for element in elements]
+    print(resultats)
+    charger(resultats)
+
+if __name__ == "__main__":
+    etl("https://www.gov.uk/search/news-and-communications")
